8630df9d8dd28d8fcdbb05f3e4fa104d7832fc59
Tatia Gabunia <tatia.gabunia.1@btu.edu.ge>
2022-12-03 23:28:26 +0400

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = firstFragment()
+        val secondFragment = secondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fragments/secondFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [secondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class secondFragment : Fragment(R.layout.fragment_second) {
+
+
+}
